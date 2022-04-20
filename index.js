6f34e9d4cba032677fc9b99c674527f08f29646b
alsaparov <saparoff.alik@gmail.com>
2022-04-21 02:55:06 +0300

    added const.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
+const text = require('./const')
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}`))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => ctx.reply(text.commands))
 bot.launch()
 
 // Enable graceful stop
